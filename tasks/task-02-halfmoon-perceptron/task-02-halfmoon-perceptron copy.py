4194c108e26b26a55c47d1dc89fa261b82f7efc1
CarlosALPessoa <carlosaleanndro@gmail.com>
2025-08-27 10:46:50 -0300

    recovery tasks

--- tasks/task-02-halfmoon-perceptron/task-02-halfmoon-perceptron copy.py
+++ tasks/task-02-halfmoon-perceptron/task-02-halfmoon-perceptron copy.py
@@ -1,12 +1,10 @@
-#%%
 import numpy as np
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt # type: ignore
 from sklearn.model_selection import train_test_split
 
-#%%
 class Perceptron:
-    def __init__(self, seed=0, input_size=2, learning_rate=0.01, epochs=100):
+    def __init__(self, seed=0, input_size=2, learning_rate=0.1, epochs=100):
         self.seed = seed
         self.learning_rate = learning_rate
         self.epochs = epochs
@@ -17,28 +15,37 @@ class Perceptron:
         rng = np.random.default_rng(self.seed)
         ### START CODE HERE ###
         ### TODO: Initialize weights with small Gaussian noise using rng.normal
-        pass
+        self.weights = rng.normal(loc=0.0, scale=0.01, size=(self.input_size + 1))
         ### END CODE HERE ###
 
     def activation(self, x):
         ### START CODE HERE ###
         ### TODO: Implement the step activation function
-        pass
+        return np.where(x >= 0, 1, -1)
         ### END CODE HERE ###
 
     def predict(self, X):
         ### START CODE HERE ###
         ### TODO: Add bias term, compute dot product with weights, apply activation
-        pass
+        X_with_bias = np.column_stack([np.ones(X.shape[0]), X])
+        return self.activation(np.dot(X_with_bias, self.weights))
         ### END CODE HERE ###
 
     def fit(self, X, y):
         ### START CODE HERE ###
         ### TODO: Implement the perceptron learning algorithm
-        pass
+        X_bias = np.insert(X, 0, 1, axis=1)
+
+        for epoch in range(self.epochs):
+            for x_i, y_true in zip(X_bias, y):
+                dot_product = np.dot(x_i, self.weights)
+                y_pred = 1 if dot_product >= 0 else -1
+
+                if y_pred != y_true:
+                    update = self.learning_rate * (y_true - y_pred) * x_i
+                    self.weights += update
         ### END CODE HERE ###
 
-#%%
 def generate_halfmoon(seed = 0,
                       n_samples=1000,
                       rad_min=50,
